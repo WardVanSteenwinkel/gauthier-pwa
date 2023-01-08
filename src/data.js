1de2ac5fdf58f612ee19a1a7da983dbc06de409f
WardVanSteenwinkel <r0856666@student.thomasmore.be>
2023-01-08 11:43:20 +0100

    images offline

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export const PRALINES_DATA=[
-    {id: 1, name: "manon fondant", color: "zwart", description: "Zachte praline, gezouten caramel en afgewerkt met een hele okkernoot.", img: "images/271234155_319699276749957_4000328248914636269_n.jpg", fillings: ["praline", "caramel"]},
-    {id: 2, name: "manon wit", color: "wit", description: "Deze klassieker bevat zachte botercreme van koffie op een fijn laagje marsepein afgewerkt met een hele okkernoot.", img: "images/271246139_683325369323589_2631719329763747776_n (1).jpg", fillings: ["botercreme", "marsepein"]},
-    {id: 3, name: "blaadje", color: "zwart", description: "Pistachemarsepein met rum omringd door intense zwarte chocolade.", img: "images/271257214_256375349797735_6472052609466035286_n.jpg", fillings: ["groene marsepein"]},
-    {id: 4, name: "kroontje", color: "zwart", description: "Dit kroontje is de ideale mix tussen intense zwarte chocolade en zoute caramel.", img: "images/271276791_508157460502944_7127541849248175643_n (1).jpg", fillings: ["praline", "caramel"]},
-    {id: 5, name: "ganache", color: "melk", description: "Zelfgemaakte melkchocolade met ganache van pure chocolade.", img: "images/271383545_447786283705312_6943723555794085727_n.jpg", fillings: ["ganache"]},
-    {id: 6, name: "aardbei", color: "melk", description: "Onder deze praline schuilt een heerlijke combinatie tussen ambachtelijke aardbeiconfituur en zachte botercreme.", img: "images/271408883_476932437343657_3158691092677106472_n.jpg", fillings: ["botercreme", "confituur"]},
-    {id: 7, name: "bol wit", color: "wit", description: "Een smeuïge vulling van botercrème en advocaat.", img: "images/271440657_3077824709200500_4383051319098221704_n.jpg", fillings: ["botercreme", "advocaat"]},
-    {id: 8, name: "farao", color: "melk", description: "Praline, caramel en nougatine omringd door heerlijke melkchocolade.", img: "images/271483798_312759560865425_7836681827812214449_n (1).jpg", fillings: ["praline", "caramel", "nougatine"]},
-    {id: 9, name: "sinaas", color: "wit", description: "Deze opvallende praline bevat ambachtelijke sinaasappelconfituur, en zachte botercreme.", img: "images/271512681_660544671765545_8066957524553581099_n.jpg", fillings: ["botercreme", "confituur"]}
+    {id: 1, name: "manon fondant", color: "zwart", description: "Zachte praline, gezouten caramel en afgewerkt met een hele okkernoot.", img: require("./images/271234155_319699276749957_4000328248914636269_n.jpg"), fillings: ["praline", "caramel"]},
+    {id: 2, name: "manon wit", color: "wit", description: "Deze klassieker bevat zachte botercreme van koffie op een fijn laagje marsepein afgewerkt met een hele okkernoot.", img: require("./images/271246139_683325369323589_2631719329763747776_n (1).jpg"), fillings: ["botercreme", "marsepein"]},
+    {id: 3, name: "blaadje", color: "zwart", description: "Pistachemarsepein met rum omringd door intense zwarte chocolade.", img: require("./images/271257214_256375349797735_6472052609466035286_n.jpg"), fillings: ["groene marsepein"]},
+    {id: 4, name: "kroontje", color: "zwart", description: "Dit kroontje is de ideale mix tussen intense zwarte chocolade en zoute caramel.", img: require("./images/271276791_508157460502944_7127541849248175643_n (1).jpg"), fillings: ["praline", "caramel"]},
+    {id: 5, name: "ganache", color: "melk", description: "Zelfgemaakte melkchocolade met ganache van pure chocolade.", img: require("./images/271383545_447786283705312_6943723555794085727_n.jpg"), fillings: ["ganache"]},
+    {id: 6, name: "aardbei", color: "melk", description: "Onder deze praline schuilt een heerlijke combinatie tussen ambachtelijke aardbeiconfituur en zachte botercreme.", img: require("./images/271408883_476932437343657_3158691092677106472_n.jpg"), fillings: ["botercreme", "confituur"]},
+    {id: 7, name: "bol wit", color: "wit", description: "Een smeuïge vulling van botercrème en advocaat.", img: require("./images/271440657_3077824709200500_4383051319098221704_n.jpg"), fillings: ["botercreme", "advocaat"]},
+    {id: 8, name: "farao", color: "melk", description: "Praline, caramel en nougatine omringd door heerlijke melkchocolade.", img: require("./images/271483798_312759560865425_7836681827812214449_n (1).jpg"), fillings: ["praline", "caramel", "nougatine"]},
+    {id: 9, name: "sinaas", color: "wit", description: "Deze opvallende praline bevat ambachtelijke sinaasappelconfituur, en zachte botercreme.", img: require("./images/271512681_660544671765545_8066957524553581099_n.jpg"), fillings: ["botercreme", "confituur"]}
 ]
 
 export const VULLINGEN_DATA=[
